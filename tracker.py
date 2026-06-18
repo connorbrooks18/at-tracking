2fc95c794ac5ba9840c8a2ca5971aa7ced1f3fc7
Connor Brooks <connorbrooks18@icloud.com>
2026-06-18 10:23:51 -0700

    adjust camera settings, april tag detection parameters, and add 5th april tag

--- tracker.py
+++ tracker.py
@@ -5,13 +5,17 @@ from pupil_apriltags import Detector
 
 port = 1
 capture = cv2.VideoCapture(port)
+capture.set(cv2.CAP_PROP_EXPOSURE, -6) # decrease exposure
+capture.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25) # decrease auto exposure
+
+
 detector = Detector(
-   families="tag36h11",
-   nthreads=1,
+   families="tag16h5", # wrong family. need to print out actual tags 
+   nthreads=2,
    quad_decimate=1.0,
-   quad_sigma=0.0,
+   quad_sigma=0.2,
    refine_edges=1,
-   decode_sharpening=0.25,
+   decode_sharpening=0.5,
    debug=0
 )
 
@@ -26,6 +30,7 @@ while(True):
 
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	tags = detector.detect(gray, estimate_tag_pose=False)
+	tags = [t for t in tags if t.decision_margin > 50]
 	for tag in tags:
 		print(f"ID: {tag.tag_id}  center: {tag.center}")
 		# draw it
